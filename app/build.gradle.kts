c412129da990ab2804e9c39747efa6dfbfab37b5
Jose Carlos Joao Moreno Aleman <joao.moreno.aleman@gmail.com>
2023-12-13 09:29:32 -0500

    implementation of the lifecycle-viewmodel library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.systemfailed.calculatorapp"
-        minSdk = 33
+        minSdk = 30
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -66,4 +66,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 }
